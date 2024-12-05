af0bc2bec5bd501aac1b510453f132fc13eac597
Sitthata <aprils96950@gmail.com>
2024-12-05 15:30:59 +0700

    fetch and attach category data to user recipes in UserRecipes component

--- src/views/recipes/recipes[userId]/UserRecipes.vue
+++ src/views/recipes/recipes[userId]/UserRecipes.vue
@@ -7,7 +7,7 @@
 <script setup>
 import RecipeCard from '@/components/RecipeCard.vue'
 import db from '@/firebase'
-import { collection, getDocs, query, where } from 'firebase/firestore'
+import { collection, getDocs, query, where, doc as document, getDoc } from 'firebase/firestore'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
@@ -21,13 +21,26 @@ async function getRecipes() {
   const qry = query(recipesCollection, where('authorId', '==', userId))
   const recipesSnapshot = await getDocs(qry)
 
-  recipes.value = recipesSnapshot.docs.map((doc) => {
-    return {
+  recipes.value = []
+
+  for (const doc of recipesSnapshot.docs) {
+    const recipe = {
       id: doc.id,
       ...doc.data(),
     }
-  })
-  console.log(recipes.value)
+
+    const categoryRef = document(db, 'categories', recipe.categoryId)
+    const categorySnap = await getDoc(categoryRef)
+    if (categorySnap.exists()) {
+      // Attach category data to the recipe
+      recipe.category = { id: categorySnap.id, ...categorySnap.data() }
+    } else {
+      // Handle cases where the category doesn't exist
+      recipe.category = null
+    }
+
+    recipes.value.push(recipe)
+  }
 }
 
 onMounted(() => {
